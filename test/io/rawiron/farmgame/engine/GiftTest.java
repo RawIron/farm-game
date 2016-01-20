f7038101e3b1f6442af851781f77b27df20a1c68
RawIron <manianology@gmail.com>
2016-01-20 17:06:14 +0100

    one test

---
+++ test/io/rawiron/farmgame/engine/GiftTest.java
@@ -0,0 +1,16 @@
+package io.rawiron.farmgame.engine;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+
+public class GiftTest extends TestCase {
+
+    @Test
+    public void testSell() throws Exception {
+        assertTrue(true);
+    }
+}
